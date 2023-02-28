8dd26e5620a72b5a26e5b19cc9d5bd3ce4c81105
romicht <tsuprikr89@gmail.com>
2023-02-28 20:43:55 +0300

    add tableView

---
+++ listOfGames/listOfGames/Modules/Common/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  listOfGames
+//
+//  Created by Роман Цуприк on 28.02.23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
