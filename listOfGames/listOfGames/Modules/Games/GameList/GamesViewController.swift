173f877552ddeb6a0d28c73557e90e9d3d3c64e6
romicht <tsuprikr89@gmail.com>
2023-03-02 20:46:36 +0300

    create URLSession

--- listOfGames/listOfGames/Modules/Games/GameList/GamesViewController.swift
+++ listOfGames/listOfGames/Modules/Games/GameList/GamesViewController.swift
@@ -12,6 +12,11 @@ class GamesViewController: UIViewController {
     let myTableView = UITableView()
     let data = ["Chess", "Domino", "Go", "Backgammon", "Sudoku"]
     let identifire = "MyCell"
+        
+//    let sessionConfiguration = URLSessionConfiguration.default
+//    let session = URLSession.shared
+//    let decoder = JSONDecoder()
+    
     
     // MARK: - Life cycle
     override func viewDidLoad() {
@@ -22,8 +27,47 @@ class GamesViewController: UIViewController {
         myTableView.delegate = self
         myTableView.dataSource = self
         self.view.addSubview(myTableView)
+        getRequest()
     }
-}
+        
+        
+//        guard let url1 = URL(string: urlString) else { return }
+//        URLSession.shared.dataTask(with: url1) { data, response, error in
+//            if let error = error {
+//                print(error)
+//                return
+//            }
+//            guard let data = data else { return }
+//            let jsonString = String(data: data, encoding: .utf8)
+//            debugPrint(jsonString!)
+        
+//            do {
+//                let decoder = JSONDecoder()
+//            let fullList = try decoder.decode([Lesson].self, from: data)
+//                print(fullList)
+////                print(fullList.first?.comments?.first?.user.name)
+//            }catch {
+//                print(error)
+//            }
+    //MARK: - Methods
+        func getRequest() {
+            let urlString = "https://api.rawg.io/api/games"
+            guard let url = URL(string: urlString) else { return }
+            var request = URLRequest(url: url)
+            
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                guard let data = data else { return }
+                let jsonString = String(data: data, encoding: .utf8)
+                print(jsonString)
+            }.resume()
+        }
+    }
+
 
 // MARK: - Extension
 extension GamesViewController: UITableViewDelegate, UITableViewDataSource {
---
+++ listOfGames/listOfGames/Modules/Games/GameList/Model.swift
@@ -0,0 +1,10 @@
+//
+//  Model.swift
+//  listOfGames
+//
+//  Created by Роман Цуприк on 1.03.23.
+//
+
+import Foundation
+
+
